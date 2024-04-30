a590d79e2b361b475092f28e0711ae9da862ee65
forseaest <u3z@naver.com>
2024-04-30 18:55:31 +0900

    feat: DecodingError 익스텐션에 localizedDescription 프로퍼티 추가

---
+++ Diary/DecodingErrorExtension.swift
@@ -0,0 +1,25 @@
+//
+//  DecodingErrorExtension.swift
+//  Diary
+//
+//  Created by H on 4/30/24.
+//
+
+import Foundation
+
+extension DecodingError {
+    var localizedDescription: String {
+        switch self {
+        case .dataCorrupted:
+            return "데이터가 손상되었습니다."
+        case let .keyNotFound(codingKey, context):
+            return "\(codingKey) 값을 찾지 못했습니다."
+        case let .valueNotFound(value, context):
+            return "\(value) 값을 찾지 못했습니다."
+        case let .typeMismatch(type, context):
+            return "\(type)이/가 일치하지 않습니다."
+        @unknown default:
+            return "알 수 없는 디코딩 에러가 발생했습니다."
+        }
+    }
+}
